9532be096f745c1be1e4339633b0981f6dbb0a49
Lucas Willian <112113982+NailliwLaw@users.noreply.github.com>
2024-08-12 15:43:48 -0300

    Implementação das rotas e novas páginas HomePage e Servicos
    - Adição do `react-router-dom` para gerenciamento de rotas.
    - Implementação do componente HomePage e adição da rota correspondente (/pageHome).
    - Implementação do componente Servicos e adição da rota correspondente (/servicos).
    - Atualização do componente Header para incluir links de navegação para as novas rotas.

---
+++ src/components/servicos/servicos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Servicos = () => {
+  return (
+    <div>
+      <h1>Nossos Serviços</h1>
+      <p>Aqui você encontra a descrição dos serviços que oferecemos.</p>
+    </div>
+  );
+};
+
+export default Servicos;

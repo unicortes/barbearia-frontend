1da467e1ddf8c1b93536272b35f49d75c0b20136
IBORD <netinho.rec@hotmail.com>
2024-08-12 19:31:44 -0300

    feat: homePage cards

--- src/components/homePage/homePage.jsx
+++ src/components/homePage/homePage.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import * as React from "react"
+import { FaCartPlus } from "react-icons/fa";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Bem-vindo à HomePage!</h1>
-    </div>
-  );
-};
+    <div class="flex gap-6">
+    <><><Card className="w-[250px]">
+      <CardHeader>
+        <CardTitle>Cadastrar Barbeiro</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-center">
+        <Button>Cadastrar</Button>
+      </CardFooter>
+    </Card><Card className="w-[250px]">
+        <CardHeader>
+          <CardTitle>Cadastrar serviço</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Cadastrar</Button>
+        </CardFooter>
+      </Card></><Card className="w-[250px]">
+        <CardHeader>
+          <CardTitle>Gerenciar estoque</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Gerenciar</Button>
+        </CardFooter>
+      </Card><Card className="w-[250px]">
+        <CardHeader >
+          <CardTitle> Cadastrar produto </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <Button>Cadastrar</Button>
+        </CardFooter>
+      </Card></>
+      </div>
+      
+    
+  )
+}
 
 export default HomePage;

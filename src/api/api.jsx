4a050a18f85b60395170ebb04c6523ca1b7a5d07
Mayara <mayeufraferreira@gmail.com>
2024-08-19 19:28:38 -0300

    update: conexão do front de produtos com o back

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,11 @@
+// lib/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://barbeariaUnicortes.com/api' // Substitua pelo URL base da sua API
+    baseURL: 'http://localhost:8080/barbeariaUnicortes/api', // URL base do seu back-end
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default api;

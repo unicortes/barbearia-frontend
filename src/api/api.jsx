11bd1ee1de8837808e893f6cc12c449bc0abf71b
Moonin371 <luanabrito285@gmail.com>
2024-08-23 12:20:06 -0300

    Alterações em Card

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://barbearia-kgd6.onrender.com/barbeariaUnicortes/api',
+    baseURL: 'http://localhost:8089/barbeariaUnicortes/api',
     headers: {
         'Content-Type': 'application/json',
     },
--- src/components/loyaltyCard/loyaltyCard.jsx
+++ src/components/loyaltyCard/loyaltyCard.jsx
@@ -1,20 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import api from '@/api/api';
-import { Table, TableHeader, TableHead, TableBody, TableRow, TableCell } from "@/components/ui/table";
+import React, { useState, useEffect } from "react";
+import api from "@/api/api";
+import {
+  Table,
+  TableHeader,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
-import { Trash2, Edit } from 'lucide-react';
+import { Trash2, Edit } from "lucide-react";
 
 const LoyaltyCard = () => {
   const [cards, setCards] = useState();
   const [newCard, setNewCard] = useState({
-    id: '',
-    date: '',
-    cardNumber: '',
-    customerName: '',
-    points: ''
+    id: "",
+    admissionDate: "",
+    client: "",
+    servicesAquired: "",
   });
   const [editMode, setEditMode] = useState(false);
   const [errors, setErrors] = useState({});
@@ -26,10 +32,10 @@ const LoyaltyCard = () => {
 
   const fetchCard = async () => {
     try {
-      const response = await api.get('/loyaltyCards'); 
+      const response = await api.get("/loyaltyCards");
       setCards(Array.isArray(response.data) ? response.data : []);
     } catch (error) {
-      console.error('Error fetching loyaltyCards:', error);
+      console.error("Error fetching loyaltyCards:", error);
     }
   };
 
@@ -37,20 +43,17 @@ const LoyaltyCard = () => {
     const { name, value } = e.target;
     setNewCard((prevCard) => ({
       ...prevCard,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const validateFields = () => {
     const errors = {};
-    if (!newCard.cardNumber) {
-      errors.cardNumber = "Número do cartão é obrigatório.";
+    if (!newCard.client) {
+      errors.client = "Nome do cliente é obrigatório.";
     }
-    if (!newCard.customerName) {
-      errors.customerName = "Nome do cliente é obrigatório.";
-    }
-    if (!newCard.points) {
-      errors.points = "Pontos são obrigatórios.";
+    if (!newCard.servicesAquired) {
+      errors.servicesAquired = "Pontos são obrigatórios.";
     }
     return errors;
   };
@@ -68,16 +71,17 @@ const LoyaltyCard = () => {
       if (editMode) {
         await api.put(`/loyaltyCards/${newCard.id}`, CardData);
       } else {
-        await api.post('/loyaltyCards', CardData);
+        console.log(CardData);
+        await api.post("/loyaltyCards/newLoyaltyCard", CardData);
       }
 
       fetchCard();
-      setNewCard({ id: '', cardNumber: '', customerName: '', points: ''});
+      setNewCard({ id: "", client: "", servicesAquired: "" });
       setEditMode(false);
       setErrors({});
       setIsModalOpen(false);
     } catch (error) {
-      console.error('Error adding/editing loyaltyCards:', error);
+      console.error("Error adding/editing loyaltyCards:", error);
     }
   };
 
@@ -86,12 +90,12 @@ const LoyaltyCard = () => {
       await api.delete(`/loyaltyCards/${id}`);
       fetchCard();
     } catch (error) {
-      console.error('Erro ao remover loyaltyCards:', error);
+      console.error("Erro ao remover loyaltyCards:", error);
     }
   };
 
   const openModalForNewCard = () => {
-    setNewCard({ id: '', cardNumber: '', customerName: '', points: ''});
+    setNewCard({ id: "", client: "", servicesAquired: "" });
     setEditMode(false);
     setIsModalOpen(true);
   };
@@ -103,17 +107,15 @@ const LoyaltyCard = () => {
   };
 
   return (
-    <div className='p-6 max-w-4xl mx-auto space-y-4 w-full'>
+    <div className="p-6 max-w-4xl mx-auto space-y-4 w-full">
       <Link to="/">
         <IoIosArrowBack className="mr-2 text-lg cursor-pointer" />
       </Link>
-      <h1 className='text-3xl font-bold'>Cartão</h1>
-      <div className='flex justify-end w-full mb-4'>
-        <Button onClick={openModalForNewCard}>
-          Adicionar produto
-        </Button>
+      <h1 className="text-3xl font-bold">Cartão</h1>
+      <div className="flex justify-end w-full mb-4">
+        <Button onClick={openModalForNewCard}>Adicionar produto</Button>
       </div>
-      <div className='border rounded w-full'>
+      <div className="border rounded w-full">
         <Table>
           <TableHeader>
             <TableRow>
@@ -124,30 +126,30 @@ const LoyaltyCard = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.isArray(cards) && cards.map((row) => (
-              <TableRow key={row.id}>
-                <TableCell>{row.id}</TableCell>
-                <TableCell>{row.cardNumber}</TableCell>
-                <TableCell>{row.customerName}</TableCell>
-                <TableCell>{row.points}</TableCell>
-                <TableCell>
-                  <div className="flex space-x-2">
-                    <button 
-                      className="text-blue-500 hover:text-blue-700"
-                      onClick={() => openModalForEditCard(row)}
-                    >
-                      <Edit className="w-4 h-4" />
-                    </button>
-                    <button 
-                      className="text-red-500 hover:text-red-700"
-                      onClick={() => handleRemoveCard(row.id)}
-                    >
-                      <Trash2 className="w-4 h-4" />
-                    </button>
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
+            {Array.isArray(cards) &&
+              cards.map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell>{row.id}</TableCell>
+                  <TableCell>{row.client}</TableCell>
+                  <TableCell>{row.servicesAquired}</TableCell>
+                  <TableCell>
+                    <div className="flex space-x-2">
+                      <button
+                        className="text-blue-500 hover:text-blue-700"
+                        onClick={() => openModalForEditCard(row)}
+                      >
+                        <Edit className="w-4 h-4" />
+                      </button>
+                      <button
+                        className="text-red-500 hover:text-red-700"
+                        onClick={() => handleRemoveCard(row.id)}
+                      >
+                        <Trash2 className="w-4 h-4" />
+                      </button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </div>
@@ -155,40 +157,64 @@ const LoyaltyCard = () => {
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-lg max-w-md w-full">
-            <h2 className="text-xl font-bold mb-4">{editMode ? 'Editar Produto' : 'Adicionar Produto'}</h2>
+            <h2 className="text-xl font-bold mb-4">
+              {editMode ? "Editar Produto" : "Adicionar Produto"}
+            </h2>
             <div className="space-y-4">
               <div>
-                <label htmlFor="cardNumber" className="block text-sm font-medium text-gray-700">Número</label>
+                <label
+                  htmlFor="client"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Nome
+                </label>
                 <Input
-                  name="cardNumber"
-                  placeholder='Número do cartão'
-                  value={newCard.cardNumber}
+                  name="client"
+                  placeholder="Nome do cliente"
+                  value={newCard.client}
                   onChange={handleInputChange}
-                  className={errors.cardNumber ? 'border-red-500' : ''}
+                  className={errors.client ? "border-red-500" : ""}
                 />
-                {errors.cardNumber && <p className='text-red-500'>{errors.cardNumber}</p>}
+                {errors.client && (
+                  <p className="text-red-500">{errors.client}</p>
+                )}
               </div>
               <div>
-                <label htmlFor="customerName" className="block text-sm font-medium text-gray-700">Nome</label>
+                <label
+                  htmlFor="servicesAquired"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Pontos
+                </label>
                 <Input
-                  name="customerName"
-                  placeholder='Nome do cliente'
-                  value={newCard.customerName}
+                  name="servicesAquired"
+                  placeholder="Pontos"
+                  value={newCard.servicesAquired}
                   onChange={handleInputChange}
-                  className={errors.customerName ? 'border-red-500' : ''}
+                  className={errors.servicesAquired ? "border-red-500" : ""}
                 />
-                {errors.customerName && <p className='text-red-500'>{errors.customerName}</p>}
+                {errors.servicesAquired && (
+                  <p className="text-red-500">{errors.servicesAquired}</p>
+                )}
               </div>
               <div>
-                <label htmlFor="points" className="block text-sm font-medium text-gray-700">Pontos</label>
+                <label
+                  htmlFor="admissionDate"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Data de Admissão
+                </label>
                 <Input
-                  name="points"
-                  placeholder='Pontos'
-                  value={newCard.points}
+                  type="date"
+                  name="admissionDate"
+                  placeholder="Data de expiração"
+                  value={newCard.admissionDate}
                   onChange={handleInputChange}
-                  className={errors.points ? 'border-red-500' : ''}
+                  className={errors.admissionDate ? "border-red-500" : ""}
                 />
-                {errors.points && <p className='text-red-500'>{errors.points}</p>}
+                {errors.admissionDate && (
+                  <p className="text-red-500">{errors.admissionDate}</p>
+                )}
               </div>
             </div>
             <div className="mt-6 flex justify-end space-x-4">
@@ -196,7 +222,7 @@ const LoyaltyCard = () => {
                 Cancelar
               </Button>
               <Button onClick={handleAddOrEditProduct}>
-                {editMode ? 'Salvar' : 'Adicionar'}
+                {editMode ? "Salvar" : "Adicionar"}
               </Button>
             </div>
           </div>

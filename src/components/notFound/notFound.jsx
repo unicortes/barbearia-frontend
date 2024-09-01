fa2efe194998e27335d5ec1f53a69f9782787785
yasmiinmuniz <yasmiindasilvamuniz@gmail.com>
2024-09-01 12:30:33 -0300

    feat: - Adicionado o novo componente `NotFound` para gerenciar rotas que não correspondem a nenhum caminho definido

---
+++ src/components/notFound/notFound.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="text-4xl font-bold mb-4">404</h1>
+      <p className="text-lg mb-8">Página não encontrada</p>
+      <Link to="/" className="text-blue-500 hover:underline">
+        Voltar para a página inicial
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;

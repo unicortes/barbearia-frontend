eea0398cac7aa9e6796d83f33911acff3ab9d4ce
yasmiinmuniz <yasmiindasilvamuniz@gmail.com>
2024-09-01 12:35:12 -0300

    Update: Atualiza HomePage com acesso condicionado para diferentes roles de usuário (ADMIN, BARBER, CLIENT) e melhorias na navegação e gerenciamento de conteúdo específico para cada role.

--- src/components/homePage/homePage.jsx
+++ src/components/homePage/homePage.jsx
@@ -1,22 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
-import { FaCalendarAlt, FaClock } from 'react-icons/fa';
-import { User, Scissors, ClipboardList, Package, CreditCard, Megaphone } from 'lucide-react';
+import { FaCalendarAlt, FaClock } from "react-icons/fa";
+import {
+  User,
+  Scissors,
+  ClipboardList,
+  Package,
+  CreditCard,
+  Megaphone,
+} from "lucide-react";
 
 const HomePage = () => {
-  const userRole = localStorage.getItem('userRole'); // Obtém o papel do usuário
+  const userRole = localStorage.getItem("userRole"); // Obtém o papel do usuário
 
   return (
     <div className="flex flex-col items-center p-6">
       <div className="grid grid-cols-3 gap-6 w-full max-w-6xl">
-        {/* Mudar para admin quando estiver tudo implementado */}
-        {userRole === null && (
+        {userRole === "ADMIN" && (
           <>
             <Link to="/barber" className="w-full">
               <Card className="h-[180px] flex flex-col justify-between p-4">
                 <CardHeader className="flex flex-col items-center flex-grow">
-                  <User className="text-gray-800 w-16 h-16" /> 
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Gerenciar Barbeiros</CardTitle>
+                  <User className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Gerenciar Barbeiros
+                  </CardTitle>
                 </CardHeader>
               </Card>
             </Link>
@@ -24,8 +32,10 @@ const HomePage = () => {
             <Link to="/services" className="w-full">
               <Card className="h-[180px] flex flex-col justify-between p-4">
                 <CardHeader className="flex flex-col items-center flex-grow">
-                  <Scissors className="text-gray-800 w-16 h-16" /> 
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Gerenciar Serviços</CardTitle>
+                  <Scissors className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Gerenciar Serviços
+                  </CardTitle>
                 </CardHeader>
               </Card>
             </Link>
@@ -34,7 +44,9 @@ const HomePage = () => {
               <Card className="h-[180px] flex flex-col justify-between p-4">
                 <CardHeader className="flex flex-col items-center flex-grow">
                   <ClipboardList className="text-gray-800 w-16 h-16" />
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Gerenciar Estoque</CardTitle>
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Gerenciar Estoque
+                  </CardTitle>
                 </CardHeader>
               </Card>
             </Link>
@@ -42,8 +54,10 @@ const HomePage = () => {
             <Link to="/products" className="w-full">
               <Card className="h-[180px] flex flex-col justify-between p-4">
                 <CardHeader className="flex flex-col items-center flex-grow">
-                  <Package className="text-gray-800 w-16 h-16" /> 
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Gerenciar Produtos</CardTitle>
+                  <Package className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Gerenciar Produtos
+                  </CardTitle>
                 </CardHeader>
               </Card>
             </Link>
@@ -51,8 +65,10 @@ const HomePage = () => {
             <Link to="/clients" className="w-full">
               <Card className="h-[180px] flex flex-col justify-between p-4">
                 <CardHeader className="flex flex-col items-center flex-grow">
-                  <User className="text-gray-800 w-16 h-16" /> 
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Cadastrar Cliente</CardTitle>
+                  <User className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Cadastrar Cliente
+                  </CardTitle>
                 </CardHeader>
               </Card>
             </Link>
@@ -61,7 +77,9 @@ const HomePage = () => {
               <Card className="h-[180px] flex flex-col justify-between p-4">
                 <CardHeader className="flex flex-col items-center flex-grow">
                   <CreditCard className="text-gray-800 w-16 h-16" />
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Gerenciar Cartões de Fidelidade</CardTitle>
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Gerenciar Cartões de Fidelidade
+                  </CardTitle>
                 </CardHeader>
               </Card>
             </Link>
@@ -70,59 +88,78 @@ const HomePage = () => {
               <Card className="h-[180px] flex flex-col justify-between p-4">
                 <CardHeader className="flex flex-col items-center flex-grow">
                   <Megaphone className="text-gray-800 w-16 h-16" />
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Gerenciar Promoções</CardTitle>
-                </CardHeader>
-              </Card>
-            </Link>
-
-            <Link to="/avaliable-time" className="w-full">
-              <Card className="h-[180px] flex flex-col justify-between p-4">
-                <CardHeader className="flex flex-col items-center flex-grow">
-                  <FaClock className="text-gray-800 w-16 h-16" />
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Gerenciar Horários</CardTitle>
-                </CardHeader>
-              </Card>
-            </Link>
-
-            <Link to="/appointment" className="w-full">
-              <Card className="h-[180px] flex flex-col justify-between p-4">
-                <CardHeader className="flex flex-col items-center flex-grow">
-                  <FaCalendarAlt className="text-gray-800 w-16 h-16" />
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Agendamento</CardTitle>
-                </CardHeader>
-              </Card>
-            </Link>
-
-            <Link to="/appointments" className="w-full">
-              <Card className="h-[180px] flex flex-col justify-between p-4">
-                <CardHeader className="flex flex-col items-center flex-grow">
-                  <FaCalendarAlt className="text-gray-800 w-16 h-16" />
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Agendamentos</CardTitle>
-                </CardHeader>
-              </Card>
-            </Link>
-
-            <Link to="/manege-appointments" className="w-full">
-              <Card className="h-[180px] flex flex-col justify-between p-4">
-                <CardHeader className="flex flex-col items-center flex-grow">
-                  <FaCalendarAlt className="text-gray-800 w-16 h-16" />
-                  <CardTitle className="mt-2 text-center text-lg font-semibold">Agendamentos</CardTitle>
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Gerenciar Promoções
+                  </CardTitle>
                 </CardHeader>
               </Card>
             </Link>
           </>
         )}
-        
-        {userRole === 'client' && (
-          // Exemplo - client vai ter acesso apenas a agendamento e historico de serviços
-          <Link to="/services" className="w-full">
-            <Card className="h-[180px] flex flex-col justify-between p-4">
-              <CardHeader className="flex flex-col items-center flex-grow">
-                <Scissors className="text-gray-800 w-16 h-16" /> 
-                <CardTitle className="mt-2 text-center text-lg font-semibold">Ver Serviços</CardTitle>
-              </CardHeader>
-            </Card>
-          </Link>
+
+        {userRole === "BARBER" && (
+          <>
+            <div className="flex justify-center w-full">
+              <Link to="/appointments" className="w-full max-w-sm">
+                <Card className="h-[180px] flex flex-col justify-between p-4">
+                  <CardHeader className="flex flex-col items-center flex-grow">
+                    <FaCalendarAlt className="text-gray-800 w-16 h-16" />
+                    <CardTitle className="mt-2 text-center text-lg font-semibold">
+                      Gerenciar Agendamentos
+                    </CardTitle>
+                  </CardHeader>
+                </Card>
+              </Link>
+            </div>
+          </>
+        )}
+
+        {userRole === "CLIENT" && (
+          <>
+            <Link to="/services" className="w-full">
+              <Card className="h-[180px] flex flex-col justify-between p-4">
+                <CardHeader className="flex flex-col items-center flex-grow">
+                  <Scissors className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Ver Serviços
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            </Link>
+
+            <Link to="/appointments/schedule" className="w-full">
+              <Card className="h-[180px] flex flex-col justify-between p-4">
+                <CardHeader className="flex flex-col items-center flex-grow">
+                  <FaCalendarAlt className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Agendar Serviços
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            </Link>
+
+            <Link to="/appointments/history" className="w-full">
+              <Card className="h-[180px] flex flex-col justify-between p-4">
+                <CardHeader className="flex flex-col items-center flex-grow">
+                  <FaCalendarAlt className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Histórico de Serviços
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            </Link>
+
+            <Link to="/appointments/manage" className="w-full">
+              <Card className="h-[180px] flex flex-col justify-between p-4">
+                <CardHeader className="flex flex-col items-center flex-grow">
+                  <FaCalendarAlt className="text-gray-800 w-16 h-16" />
+                  <CardTitle className="mt-2 text-center text-lg font-semibold">
+                    Gerenciar Agendamentos
+                  </CardTitle>
+                </CardHeader>
+              </Card>
+            </Link>
+          </>
         )}
       </div>
     </div>

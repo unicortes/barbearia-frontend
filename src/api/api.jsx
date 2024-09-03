4e90b77ff0449f6c19de7c93a460b609980be492
Matheus Marcos Joel da Silva <70113618+Matheusxr77@users.noreply.github.com>
2024-09-03 10:07:28 -0300

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://barbearia-kgd6.onrender.com/barbeariaUnicortes',
+    baseURL: 'http://barbearia-kgd6.onrender.com/barbeariaUnicortes',
     headers: {
         'Content-Type': 'application/json',
     },
